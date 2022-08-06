e31667b2dd4f0d6383140a02a5c45d5fc5262780
MatiasMontenegrom <matiasmontenegrom@gmail.com>
2022-08-06 21:03:26 +0000

    commit

--- boilerplate-express-main/myApp.js
+++ boilerplate-express-main/myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
 console.log("Hello World")
 
 app.use("/public", express.static(__dirname + "/public"));
@@ -9,7 +10,11 @@ app.get('/', (req, res) => {
   })
 
 app.get('/json',(req,res)=>{
-    res.json({"message": "Hello json"})
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        res.json({"message": "HELLO JSON"})
+      } else {
+        res.json({"message": "Hello json"})
+      }
 })
 
 
