b2746f1bd21d2c83b6cf6da5d88c81fa5e6c7b60
MatiasMontenegrom <matiasmontenegrom@gmail.com>
2022-08-08 13:50:50 +0000

    commit

--- boilerplate-express-main/myApp.js
+++ boilerplate-express-main/myApp.js
@@ -24,6 +24,19 @@ app.get('/json',(req,res)=>{
       }
 })
 
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.send({
+      time: req.time
+    });
+  }
+);
+
 
 
 
